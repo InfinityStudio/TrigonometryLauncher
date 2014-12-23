da2e4b5546c2166d4700457f432048591d79dd73
DeckerCHAN <DeckerCHAN@gmail.com>
2014-12-23 11:16:54 +0800

    Added hello world

---
+++ src/main/java/net/teamtf/launcher/Start.java
@@ -0,0 +1,11 @@
+package net.teamtf.launcher;
+
+/**
+ *
+ * @author Decker
+ */
+public class Start {
+      public static void main(String[] args) {
+          System.out.println("Hello world!");
+    }
+}
