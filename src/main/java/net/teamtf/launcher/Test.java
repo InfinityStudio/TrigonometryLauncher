fff066373631b7c67138a5841bc24026c5fa06e6
DeckerCHAN <DeckerCHAN@gmail.com>
2014-12-25 13:44:22 +0800

    Added log function Now we could using "Logger logger = LogManager.getLogger(theClass.class.getName());" to get Logger and log message through 'logger.info("some message");'

--- src/main/java/net/teamtf/launcher/Test.java
+++ src/main/java/net/teamtf/launcher/Test.java
@@ -1,11 +1,19 @@
 package net.teamtf.launcher;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  *
  * @author Decker
  */
 public class Test {
+
     public static void main(String[] args) {
-        System.out.println("Im just testing!");
+        Log log = LogFactory.getLog(Test.class);
+        log.debug("hello!debug");
+        log.info("hello!info");
+        log.warn("hello!warn");
+        log.error("hello!error");
     }
 }
