ab87a2f0b09338e8b8671d9876c47b2e8103cd03
DeckerCHAN <DeckerCHAN@gmail.com>
2014-12-26 12:20:29 +0800

    Added addon system

---
+++ src/main/java/net/teamtf/launcher/addon/AddonLoader.java
@@ -0,0 +1,21 @@
+package net.teamtf.launcher.addon;
+
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+/**
+ *
+ * @author Decker
+ */
+public final class AddonLoader {
+    private final  File addonsFolder;
+    public AddonLoader(String addonsFolerPath) {
+        if (!new File(addonsPath).isDirectory()) {
+            throw new IllegalArgumentException("path is not a directory");
+        }
+        FileU
+    }
+    
+    
+
+}
