f258298dadabeed7c3236a43d11d65375ec8e03c
DeckerCHAN <DeckerCHAN@gmail.com>
2014-12-30 19:34:58 +0800

    Refaced configuration system

--- src/main/java/net/teamtf/launcher/configuration/Config.java
+++ src/main/java/net/teamtf/launcher/configuration/Config.java
@@ -2,11 +2,12 @@ package net.teamtf.launcher.configuration;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.util.HashMap;
 import net.teamtf.launcher.core.Engine;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.yaml.snakeyaml.Yaml;
 
 /**
@@ -15,54 +16,56 @@ import org.yaml.snakeyaml.Yaml;
  */
 public class Config {
 
-    private static Config instance;
-
-    private static void init() {
-        if (instance == null) {
-            instance = new Config();
-        }
-    }
-
-    public static String getConfig(String configName) {
-        init();
-        return instance.config.get(configName);
-    }
-
-    public static void setConfig(String configName, String value) {
-        instance.config.put(configName, value);
-        instance.saveTofFile();
-    }
-
-    public static void saveAllConfigToFile() throws IOException {
-        instance.saveTofFile();
-    }
     private File configFile;
     private HashMap<String, String> config;
+    private Log configLogSystem;
 
-    private void saveTofFile() {
+    private void saveToFile() {
         try {
             String stringToFile = (String) (new Yaml().dump(this.config));
             FileUtils.writeStringToFile(configFile, stringToFile);
         } catch (Exception e) {
             Engine.getEngine().getLogger().error("Can not write config to file.", e);
         }
-
     }
 
-    public Config() {
+    public String getConfig(String key) {
+        String res = this.config.get(key);
+        if (StringUtils.isEmpty(res)) {
+            throw new IllegalAccessException("No such config");
+        }
+        return res;
+    }
+    public void setConfig(String key,String value)
+    {
+        this.config.put(key, value);
+        this.saveToFile();
+    }
+
+    public Config(String configFilePath) {
+        this.configLogSystem = LogFactory.getLog("CONFIG");
         try {
-            configFile = new File(new File(URLDecoder.decode(Engine.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "UTF-8")).getParentFile().getCanonicalPath()).toPath().resolve("config.yml").toFile();
+            //I do not know which implementation is better. 
+            //But, by following the principle of Occam's Razor, I would using simple one.
+            //If exception occures at here, replace by next implementation.
+            configFile = new File(configFilePath);
+            //configFile = new File(new File(URLDecoder.decode(Engine.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "UTF-8")).getParentFile().getCanonicalPath()).toPath().resolve(configFilePath).toFile();
+
             if (!configFile.exists()) {
                 configFile.createNewFile();
             } else {
                 String fileContent = FileUtils.readFileToString(configFile);
+                Object o = new Yaml().load(fileContent);
+                if (!(o instanceof HashMap)) {
+                    throw new IOException("Invalid file format. Please check your file format through http://www.yamllint.com/");
+                }
                 this.config = (HashMap<String, String>) (new Yaml().load(fileContent));
                 if (this.config == null) {
                     this.config = new HashMap<>();
                 }
             }
         } catch (Exception e) {
-            Engine.getEngine().getLogger().fatal("Can not process config file.");
+            this.configLogSystem.fatal("Can not pass config file.", e);
             System.exit(0);
         }
 
