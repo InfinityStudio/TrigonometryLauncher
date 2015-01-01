9afd1c99b51003be3f85edd57f3dc42065ab100c
Darkyoooooo <844199661@qq.com>
2015-01-01 12:50:48 +0800

    Code style

--- src/main/java/net/teamtf/launcher/util/task/Task.java
+++ src/main/java/net/teamtf/launcher/util/task/Task.java
@@ -1,9 +1,9 @@
 package net.teamtf.launcher.util.task;
+
 /**
  * Basic element of task management which responsible for single process.
  * @author decker
  */
-public interface Task extends Runnable{
+public interface Task extends Runnable {
     public String getName();
-    
 }
