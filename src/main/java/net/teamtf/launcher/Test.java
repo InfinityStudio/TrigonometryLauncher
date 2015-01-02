900cf267262baf0b2a34912be859b7948dbfa052
DeckerCHAN <DeckerCHAN@gmail.com>
2015-01-02 14:49:46 +0800

    Declare rules for Test.java

--- src/main/java/net/teamtf/launcher/Test.java
+++ src/main/java/net/teamtf/launcher/Test.java
@@ -1,13 +1,16 @@
 package net.teamtf.launcher;
 
-import net.teamtf.launcher.localization.I18N;
-
 /**
- *
+ * Test main functions.
+ * This is just a test for every body their self, thus, do not add this file to version control or even commit it.
+ * If you encountered problems such as require commit before you could pull. 
+ * Use "git stash" to backup your workspace and "git stash pop" after that operation.
+ * 
  * @author Decker
  */
 public class Test {
+
     public static void main(String[] args) {
-	System.out.println(I18N.get("tflauncher.core.programname"));
+        System.out.println("This is just a test");
     }
 }
