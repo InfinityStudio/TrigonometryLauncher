4d21c00b1c0c7522cdd3e224ceb487d6cce5aa9a
DeckerCHAN <DeckerCHAN@gmail.com>
2015-01-13 16:25:10 +0800

    Refactored config system. Add config.yml to default usage. I think I supposed to migrate this to commons-config maybe.

--- src/main/java/net/teamtf/launcher/configuration/Config.java
+++ src/main/java/net/teamtf/launcher/configuration/Config.java
@@ -1,19 +1,20 @@
 package net.teamtf.launcher.configuration;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-
 import net.teamtf.launcher.core.Engine;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- *
  * @author Decker
  */
 public class Config {
@@ -21,6 +22,7 @@ public class Config {
     private File configFile;
     private HashMap<String, String> config;
     private Log configLogSystem;
+    private final Pattern referencePattern;
 
     private void saveToFile() {
         try {
@@ -32,9 +34,19 @@ public class Config {
     }
 
     public String getConfig(String key) {
+        key = StringUtils.removeStart(key, "${");
+        key = StringUtils.removeEnd(key, "}");
+
         String res = this.config.get(key);
         if (StringUtils.isEmpty(res)) {
             this.configLogSystem.warn(String.format("No such key called '%s' contained in this config", key));
+        }
+        if (this.referencePattern.matcher(res).find()) {
+            Matcher matcher = this.referencePattern.matcher(res);
+            while (matcher.find()) {
+                res = matcher.replaceFirst(this.getConfig(matcher.group()));
+            }
+
         }
         return res;
     }
@@ -46,12 +58,18 @@ public class Config {
 
     @SuppressWarnings("unchecked")
     public Config(File configFile) {
+        this.referencePattern = Pattern.compile("\\$\\{.*?\\}");
         this.configLogSystem = LogFactory.getLog("CONFIG");
         try {
             this.configFile = configFile;
 
             if (!configFile.exists()) {
-                configFile.createNewFile();
+                InputStream config = getClass().getClassLoader().getResourceAsStream("defaults/config.yml");
+                FileUtils.copyInputStreamToFile(config, configFile);
+            } else if (!(new Yaml().load(FileUtils.readFileToString(configFile)) instanceof HashMap)) {
+                FileUtils.deleteQuietly(configFile);
+                InputStream config = getClass().getClassLoader().getResourceAsStream("defaults/config.yml");
+                FileUtils.copyInputStreamToFile(config, configFile);
             } else {
                 String fileContent = FileUtils.readFileToString(configFile);
                 Object o = new Yaml().load(fileContent);
--- src/main/java/net/teamtf/launcher/core/Engine.java
+++ src/main/java/net/teamtf/launcher/core/Engine.java
@@ -1,25 +1,30 @@
 package net.teamtf.launcher.core;
 
-import java.io.File;
-import java.net.URLDecoder;
-import net.teamtf.launcher.basis.GUI.DefaultUIControler;
 import net.teamtf.launcher.addon.AddonLoader;
+import net.teamtf.launcher.auth.AuthClient;
 import net.teamtf.launcher.basis.DefaultLauncher;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
+import net.teamtf.launcher.basis.gui.DefaultUIControler;
 import net.teamtf.launcher.configuration.Config;
 import net.teamtf.launcher.localization.DefaultI18n;
 import net.teamtf.launcher.localization.I18n;
+import net.teamtf.launcher.provider.AssestsProvider;
+import net.teamtf.launcher.provider.LibraryProvider;
+import net.teamtf.launcher.provider.NativeProvider;
+import net.teamtf.launcher.provider.VersionProvider;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.File;
+import java.net.URLDecoder;
 
 /**
  * @Author Decker
  */
 public class Engine {
-    
+
     private static Engine instance;
-    
+
     public static void initEngine() throws Exception {
         if (instance == null) {
             instance = new Engine();
@@ -27,11 +32,11 @@ public class Engine {
             throw new IllegalAccessException("Engine has already initialized");
         }
     }
-    
+
     public static Engine getEngine() {
         return instance;
     }
-    
+
     private final File gameDir;
     private final File launcherDir;
     private final Log logger;
@@ -40,25 +45,30 @@ public class Engine {
     private I18n i18n;
     private final AddonLoader addonLoader;
     private final Config configuration;
-    
+    private AuthClient authClient;
+    private LibraryProvider libraryProvider;
+    private AssestsProvider assestsProvider;
+    private NativeProvider nativeProvider;
+    private VersionProvider versionProvider;
+
     private Engine() throws Exception {
         this.launcherDir = new File(new File(URLDecoder.decode(Engine.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "UTF-8")).getParentFile().getCanonicalPath());
-        
+
         this.logger = LogFactory.getLog("MainLogger");
         this.configuration = new Config(FileUtils.getFile(this.launcherDir, "config.yml"));
-        
+        this.configuration.setConfig("launcher_dir", this.launcherDir.toString());
+
         this.gameDir = FileUtils.getFile(this.configuration.getConfig("game_dir"));
         this.i18n = new DefaultI18n();
-        this.uiController = new DefaultUIControler();
+        this.setUiController(new DefaultUIControler());
         this.launcher = new DefaultLauncher();
-        
+
         this.addonLoader = new AddonLoader(this.configuration.getConfig("addonfolder"));
-        
+
     }
 
     /**
      * Engine start working. 1.load add addons.
-     *
      *
      * @throws Exception
      */
@@ -67,19 +77,19 @@ public class Engine {
         //I just put everything here as possible.
         this.addonLoader.loadFilesFromFolder();
         this.addonLoader.perLoadAllAddons();
-        this.uiController.getMainWindow();
+        this.getUiController().getMainWindow();
         this.addonLoader.loadAllAddons();
-        this.uiController.getMainWindow().setVisible(true);
+        this.getUiController().getMainWindow().setVisible(true);
         this.addonLoader.postLoadAllAddons();
-        
+
     }
-    
+
     public Log getLogger() {
         return logger;
     }
-    
+
     public UIController getUIController() {
-        return this.uiController;
+        return this.getUiController();
     }
 
     /**
@@ -90,11 +100,11 @@ public class Engine {
     public Launcher getLauncher() {
         return launcher;
     }
-    
+
     public void setLauncher(Launcher launcher) {
         this.launcher = launcher;
     }
-    
+
     public AddonLoader getAddonLoader() {
         return addonLoader;
     }
@@ -105,9 +115,9 @@ public class Engine {
     public Config getEngineConfig() {
         return configuration;
     }
-    
+
     public void stop() {
-        this.uiController.closeAllWindow();
+        this.getUiController().closeAllWindow();
     }
 
     /**
@@ -137,5 +147,24 @@ public class Engine {
     public File getGameDir() {
         return gameDir;
     }
-    
+
+    public UIController getUiController() {
+        return uiController;
+    }
+
+    public AuthClient getAuthClient() {
+        return authClient;
+    }
+
+    public void setAuthClient(AuthClient authClient) {
+        this.authClient = authClient;
+    }
+
+    public LibraryProvider getLibraryProvider() {
+        return libraryProvider;
+    }
+
+    public void setLibraryProvider(LibraryProvider libraryProvider) {
+        this.libraryProvider = libraryProvider;
+    }
 }
